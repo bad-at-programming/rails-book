d49aa6d42b3c8670392e8e5387780d33a843e2ce
Mark <mark@mark-VirtualBox.(none)>
2013-11-11 20:28:00 -0500

    Update the Gemfile and improve the Readme file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-RailsBook::Application.config.secret_key_base = 'e21340a8b4cae9b3cc66af07618dbd85e553d7fadac40d95c48c877f1aed61ab9a4304a7f2b30fa69576f890a8435e702dd53d2f4a8d77bac8827e5b490a4d7e'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+RailsBook::Application.config.secret_key_base = secure_token
